f2f10d29bd0b8e031e6fcbfc59879ad5c580ddfb
Adilson Santos <chleba_08@chleba-08.local>
2020-02-18 15:18:37 -0300

    0.6.4
    - Mudança no sha-256 para compatibilidade com os navegadores

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,5 +2,3 @@ import lazysizes from "lazysizes";
 import 'lazysizes/plugins/native-loading/ls.native-loading';
 
 import instant from "instant.page";
-
-var date=(new Date).getFullYear();document.getElementById("currentYear").innerHTML=date;
