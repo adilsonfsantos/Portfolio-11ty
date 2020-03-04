7c714c5f4795b7ec3b664000dc421cc1272cd6b5
Adilson Santos <chleba_08@chleba-08.local>
2020-03-04 16:42:32 -0300

    0.10.0
    - Limpeza dos Shotcodes de imagens
    - Organização dos arquivos SCSS

--- src/_includes/components/Picture.js
+++ src/_includes/components/Picture.js
@@ -20,31 +20,3 @@ module.exports = (
 </noscript>
 `;
 };
-
-
-// <picture class="lozad ${classParent}" style="display: block; min-height: 1rem" data-iesrc="${filename}-480.jpg" data-alt="${altText}">
-//   <source srcset="${filename}-240.webp 240w, ${filename}-320.webp 320w, ${filename}-480.webp 480w, ${filename}-640.webp 640w, ${filename}-768.webp 768w, ${filename}-1280.webp 1280w" type="image/webp"></source>
-//   <source srcset="${filename}-240.jpg 240w, ${filename}-320.jpg 320w, ${filename}-480.jpg 480w, ${filename}-640.jpg 640w, ${filename}-768.jpg 768w, ${filename}-1280.jpg 1280w" type="image/jpeg">
-
-// </picture>
-// <noscript>
-//   <picture class="${classParent}">
-//     <source srcset="${filename}-240.webp 240w, ${filename}-320.webp 320w, ${filename}-480.webp 480w, ${filename}-640.webp 640w, ${filename}-768.webp 768w, ${filename}-1280.webp 1280w" type="image/webp">
-//     <source srcset="${filename}-240.jpg 240w, ${filename}-320.jpg 320w, ${filename}-480.jpg 480w, ${filename}-640.jpg 640w, ${filename}-768.jpg 768w, $ {filename}-1280.jpg 1280w" type="image/jpeg">
-//     <img class="${classDescendent}" loading="lazy" src="${filename}-480.jpg"  alt="${altText}">
-//   </picture>
-// </noscript>
-
-{/* <picture class="lazy ${classParent}">
-  <source data-srcset="${filename}-240.webp 240w, ${filename}-320.webp 320w, ${filename}-480.webp 480w, ${filename}-640.webp 640w, ${filename}-768.webp 768w, ${filename}-1280.webp 1280w" type="image/webp"></source>
-  <source data-srcset="${filename}-240.jpg 240w, ${filename}-320.jpg 320w, ${filename}-480.jpg 480w, ${filename}-640.jpg 640w, ${filename}-768.jpg 768w, ${filename}-1280.jpg 1280w" type="image/jpeg">
-  <img class="lazyload ${classDescendent}" loading="lazy" data-src="${filename}-480.jpg" src="${filename}-lq.jpg" data-sizes="auto" alt="${altText}">
-</picture>
-<noscript>
-  <picture class="${classParent}">
-    <source srcset="${filename}-240.webp 240w, ${filename}-320.webp 320w, ${filename}-480.webp 480w, ${filename}-640.webp 640w, ${filename}-768.webp 768w, ${filename}-1280.webp 1280w" type="image/webp">
-    <source srcset="${filename}-240.jpg 240w, ${filename}-320.jpg 320w, ${filename}-480.jpg 480w, ${filename}-640.jpg 640w, ${filename}-768.jpg 768w, $ {filename}-1280.jpg 1280w" type="image/jpeg">
-    <img class="${classDescendent}" loading="lazy" src="${filename}-480.jpg"  alt="${altText}">
-  </picture>
-</noscript> */}
-// data-src="${filename}-480.jpg"
--- src/_includes/components/PictureCard.js
+++ src/_includes/components/PictureCard.js
@@ -20,16 +20,3 @@ module.exports = (
 </noscript>
 `;
 };
-
-{/* <picture class="lozad ${classParent}" style="display: block; min-height: 1rem" data-iesrc="${filename}-480.jpg" data-alt="${altText}">
-<source srcset="${filename}-240.webp 240w, ${filename}-320.webp 320w, ${filename}-480.webp 480w, ${filename}-640.webp 640w" type="image/webp">
-<source srcset="${filename}-240.jpg 240w, ${filename}-320.jpg 320w, ${filename}-480.jpg 480w, ${filename}-640.jpg 640w" type="image/jpeg">
-</picture>
-<noscript>
-<picture class="${classParent}">
-  <source srcset="${filename}-240.webp 240w, ${filename}-320.webp 320w, ${filename}-480.webp 480w, ${filename}-640.webp 640w" type="image/webp">
-  <source srcset="${filename}-240.jpg 240w, ${filename}-320.jpg 320w, ${filename}-480.jpg 480w, ${filename}-640.jpg 640w" type="image/jpeg">
-  <img class="${classDescendent}" loading="lazy" src="${filename}-480.jpg"  alt="${altText}">
-</picture>
-</noscript> */}
-// data-src="${filename}-lq.jpg"
