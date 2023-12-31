06ccd915cf289f04d207845d4c7f8c7ff87612de
Adilson Santos <adilson.santos97@live.com>
2023-12-31 01:15:26 -0300

    Fix loading in banner images and wrong data
    By now the post banner images has been using the loading="lazy" method,
    but it should be using the loading="eager" because the image is on the
    viewport and must load faster. Also rewrite some variables in the image
    code to be more readable.
    
    Replaced the image in site.json with the new thumbnail file, also
    removed the behance link because it's not active.

--- src/11ty/components/images.js
+++ src/11ty/components/images.js
@@ -1,15 +1,16 @@
 const Image = require("@11ty/eleventy-img");
 
-async function images(src, alt, classParent, classDescendent, sizes) {
-  if (alt === undefined || typeof alt !== "string") {
+async function images(path, altText, classParent, classDescendent, type) {
+  if (altText === undefined || typeof altText !== "string") {
     // You bet we throw an error on missing alt (alt="" works okay)
-    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+    throw new Error(`Missing \`alt\` on image from: ${path}`);
   }
 
-  let newSizes;
-  let lowSrc;
+  let imageSizes;
+  let lowQuality;
+  let loadType;
 
-  let metadata = await Image(src, {
+  let metadata = await Image(path, {
     widths: [320, 480, 768, 1104, 1650, 1920],
     formats: ["webp", "jpeg"],
     sharpOptions: {
@@ -21,32 +22,35 @@ async function images(src, alt, classParent, classDescendent, sizes) {
     outputDir: "dist/assets/images",
   });
 
-  if (sizes === "card") {
-    newSizes =
-      "(min-width: 1260px) 470px, (min-width: 620px) calc(42.1vw - 52px), calc(92.33vw - 64px), 100vw";
-    lowSrc = metadata.jpeg[1];
-  } else if (sizes === "banner") {
-    newSizes = "(min-width: 1200px) 1104px, 87.5vw";
-    lowSrc = metadata.jpeg[2];
+  if (type === "card") {
+    imageSizes = "(min-width: 1260px) 470px, (min-width: 620px) calc(42.1vw - 52px), \
+      calc(92.33vw - 64px), 100vw";
+    lowQuality = metadata.jpeg[1];
+    loadType = "lazy";
+  } else if (type === "banner") {
+    imageSizes = "(min-width: 1200px) 1104px, 87.5vw";
+    lowQuality = metadata.jpeg[2];
+    loadType = "eager";
   } else {
-    newSizes = "(min-width: 1200px) 1104px, 87.5vw";
-    lowSrc = metadata.jpeg[2];
+    imageSizes = "(min-width: 1200px) 1104px, 87.5vw";
+    lowQuality = metadata.jpeg[2];
+    loadType = "lazy";
   }
 
   return `<picture class="${classParent}">
     ${Object.values(metadata)
       .map((imageFormat) => {
         return `<source type="${imageFormat[0].sourceType}"
-          srcset="${imageFormat.map((entry) => entry.srcset).join(", ")}" sizes="${newSizes}">`;
+          srcset="${imageFormat.map((entry) => entry.srcset).join(", ")}" sizes="${imageSizes}">`;
       })
       .join("\n")}
       <img
         class="${classDescendent}"
-        src="${lowSrc.url}"
-        width="${lowSrc.width}"
-        height="${lowSrc.height}"
-        alt="${alt}"
-        loading="lazy"
+        src="${lowQuality.url}"
+        width="${lowQuality.width}"
+        height="${lowQuality.height}"
+        alt="${altText}"
+        loading="${loadType}"
         decoding="async">
     </picture>`;
 }
